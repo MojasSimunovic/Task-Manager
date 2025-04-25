79532e93a1c81f5462a6db2cabb71c263a66280b
MojasSimunovic <mojas0402@gmail.com>
2025-04-25 15:43:54 +0200

    dynamic  class shift on active user

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,4 @@
 import { Component, computed, Input, signal, input, Output, EventEmitter } from '@angular/core';
-import { DUMMY_USERS } from '../../../dummy-users';
-
 import { User } from './user.model';
 
 
@@ -15,19 +13,9 @@ import { User } from './user.model';
 
 export class UserComponent {
 
+  @Input() active: boolean | undefined;
   @Input({required: true}) user!: User;
-
-// inline settig up the type, preferable version would be with ts type
-//  @Input({required: true}) user!: {
-//    id:string,
-//    name:string,
-//    avatar: string
-//  }
-
   @Output() select = new EventEmitter<string>();
-//  select = output<string>(); output approach, the bottom function remains the same
-//  avatar = input.required<string>();
-//  name = input.required<string>(); this is signal approach
 
   onClickEvent() {
    this.select.emit(this.user.id)
